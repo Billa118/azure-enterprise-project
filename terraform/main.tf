99e812523552f9d1f86ed42435da6df56159bf9b
Manoj Billa <billamanoj9890@gmail.com>
2026-07-15 10:31:55 +0530

    Day 7: Terraform networking and resource import

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,55 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Vnet01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+resource "azurerm_subnet" "web" {
+  name                 = "WEBsubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+
+  default_outbound_access_enabled = false
+
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = "APPsubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  default_outbound_access_enabled = false
+
+}
+
+resource "azurerm_subnet" "db" {
+  name                 = "DBsubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  default_outbound_access_enabled = false
+
+}
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = "WebNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "AppNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "DbNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
